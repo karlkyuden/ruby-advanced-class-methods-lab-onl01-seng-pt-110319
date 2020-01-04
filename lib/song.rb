7e3fa5779bf14c86ffa5e32cb9f8d9616568f20f
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-04 18:34:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,9 +23,9 @@ class Song
     song
   end
   
-  def self.find_by_name(name)
-    @@all.find{|song| song.name == name}
-  end
+  #def self.find_by_name(name)
+   # @@all.find{|song| song.name == name}
+  #end
     
     
 
